3fce3f0ac49abc852645984a387247616903e80f
Ekansh <zethyst@protonmail.com>
2024-05-22 02:33:15 +0530

    Made Some Improvements

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -5,7 +5,7 @@ import Section from '../assets/motionarteffect-img11.png'
 function Apply() {
   return (
     <div className='flex flex-col justify-center items-center px-8 md:px-32 p-10 gap-14 md:gap-32 pointer-events-none relative'>
-        <p className='text-3xl md:text-5xl font-semibold text-[#EEE5FF] text-center leading-10 md:leading-[5rem] md:w-[55%]'>Apply On Any Section Or Enable For Whole Page</p>
+        <p className='text-3xl md:text-5xl font-semibold text-[#EEE5FF] text-center leading-10 md:leading-[4.5rem] md:w-[55%]'>Apply On Any Section Or Enable For Whole Page</p>
         <div className='flex flex-col md:flex-row justify-center items-center gap-7'>
             <div className='md:-translate-y-20  flex flex-col justify-center items-center gap-7 gradient text-[#EEE5FF] p-10 md:w-[50%] rounded-2xl' style={{border:"1px solid #eee5ff31"}}>
                 <p className='capitalize text-3xl self-start font-semibold'>Apply on section</p>
